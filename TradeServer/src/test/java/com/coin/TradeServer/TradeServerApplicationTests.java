29bf8ccdf976877cb49ebc84762c4e5e475ad457
eoc940 <hgk0000@naver.com>
2022-05-28 01:41:36 +0900

    add server project

---
+++ TradeServer/src/test/java/com/coin/TradeServer/TradeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coin.TradeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
