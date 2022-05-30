5688bf7df35c1ea865c1759fc409b52b482bd582
eoc940 <hgk0000@naver.com>
2022-05-30 10:12:21 +0900

    test commit

--- TradeServer/src/main/java/com/coin/TradeServer/TradeServerApplication.java
+++ TradeServer/src/main/java/com/coin/TradeServer/TradeServerApplication.java
@@ -8,6 +8,17 @@ public class TradeServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradeServerApplication.class, args);
+		System.out.println();
 	}
+
+	public void func() {
+		System.out.println(123);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
 
 }
